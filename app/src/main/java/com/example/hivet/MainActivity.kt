3b8bd2ac5836b19cd9c365363196c36febb9ac44
afsha <seaid18523@gmail.com>
2021-12-12 16:07:52 +0700

    cobain aja

--- app/src/main/java/com/example/hivet/MainActivity.kt
+++ app/src/main/java/com/example/hivet/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.hivet
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.seaID.hivet.R
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
