a67da089682fcf8fdefa9bdb0fcebc171763c78f
afsha <18523047@students.uii.ac.id>
2022-02-18 21:15:29 +0700

    update

--- app/src/main/java/com/seaID/hivet/MainActivity.kt
+++ app/src/main/java/com/seaID/hivet/MainActivity.kt
@@ -35,6 +35,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
 
         mAuth = FirebaseAuth.getInstance()
@@ -74,6 +75,7 @@ class MainActivity : AppCompatActivity() {
 
         profile.setOnClickListener {
             startActivity(Intent(this, UserProfileActivity::class.java))
+
         }
 
 
